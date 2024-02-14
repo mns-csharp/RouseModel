df7e55e5fc7578cf68c02e0ae16f3f77b38f5618
Mohammad Saqib <m.saqib@student.uw.edu.pl>
2024-02-14 10:22:20 +0100

    degugged

--- AutocorrTesting.py
+++ AutocorrTesting.py
@@ -2,8 +2,8 @@ from framework.ComputeCForRange import ComputeCForRange
 from framework.read_vec3 import read_vec3
 
 if __name__ == "__main__":
-    r_end_vec = r"framework\test_data_2.txt"
-    dir1 = "."
+    r_end_vec = r"framework/test_data_2.txt"
+    dir1 = ""
     chain1Vec3List = read_vec3(dir_path=dir1, file_name=r_end_vec)
     print(chain1Vec3List)
     lags, autos = ComputeCForRange(chain1Vec3List, 3)
